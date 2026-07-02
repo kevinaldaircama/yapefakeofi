53d6946c2fe11afac339df5289d5b1d93bf1918d
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-02 17:23:35 -0500

    Update firebase configuration method
    Refactored firebase configuration to use global variable.

--- app/token admin.js
+++ app/token admin.js
@@ -9,20 +9,10 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";  
   
   
-const firebaseConfig = {  
-  apiKey: "AIzaSyAgynl913FF8I1x23n1HZ5pYm6pS1p1i5M",
-    authDomain: "yape-b7dd2.firebaseapp.com",
-    databaseURL: "https://yape-b7dd2-default-rtdb.firebaseio.com",
-    projectId: "yape-b7dd2",
-    storageBucket: "yape-b7dd2.firebasestorage.app",
-    messagingSenderId: "921630422374",
-    appId: "1:921630422374:web:b622038a7b4128cc6284a7",
-    measurementId: "G-0K02J87M67" 
-};  
-  
-  
-const app = initializeApp(firebaseConfig);  
-const db = getDatabase(app);  
+const firebaseConfig = window.firebaseConfig;
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
   
 let chart;
   
